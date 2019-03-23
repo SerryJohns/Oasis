0d4bcf043746de5096950360251aa79c7ef11ce3
Wagaana Alex <wagaanaa@gmail.com>
2019-03-23 04:09:07 +0300

    Apointment improved this time

--- app/src/main/java/com/oasis/oasis/BookAppointment.java
+++ app/src/main/java/com/oasis/oasis/BookAppointment.java
@@ -1,7 +1,14 @@
 package com.oasis.oasis;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class BookAppointment extends AppCompatActivity {
 
@@ -9,5 +16,35 @@ public class BookAppointment extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_appointment);
+
+        final TextView birthDay=findViewById(R.id.birthDay);
+
+        RelativeLayout pickDate=findViewById(R.id.pickDate);
+
+        pickDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getDatePicker(birthDay);
+            }
+        });
+
+    }
+
+    private void getDatePicker(final TextView birthDay){
+        AlertDialog.Builder mBuilder=new AlertDialog.Builder(this);
+        final View mView=getLayoutInflater().inflate(R.layout.date_picker, null);
+
+        DatePicker pickDate=mView.findViewById(R.id.pickDate);
+        Calendar calendar=Calendar.getInstance();
+        pickDate.init(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH), new DatePicker.OnDateChangedListener() {
+            @Override
+            public void onDateChanged(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                birthDay.setText(dayOfMonth+"/"+monthOfYear+"/"+year);
+            }
+        });
+
+        mBuilder.setView(mView);
+        AlertDialog dialog=mBuilder.create();
+        dialog.show();
     }
 }
--- app/src/main/java/com/oasis/oasis/Profile.java
+++ app/src/main/java/com/oasis/oasis/Profile.java
@@ -1,9 +1,16 @@
 package com.oasis.oasis;
 
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class Profile extends AppCompatActivity {
 
@@ -14,6 +21,37 @@ public class Profile extends AppCompatActivity {
 
         EditText fullName=findViewById(R.id.names);
         Button submitUpdates=findViewById(R.id.applyUpdate);
+        final TextView birthDay=findViewById(R.id.birthDay);
+
+        RelativeLayout pickDate=findViewById(R.id.pickDate);
+
+        pickDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getDatePicker(birthDay);
+            }
+        });
+
     }
+
+    private void getDatePicker(final TextView birthDay){
+        AlertDialog.Builder mBuilder=new AlertDialog.Builder(this);
+        final View mView=getLayoutInflater().inflate(R.layout.date_picker, null);
+
+        DatePicker pickDate=mView.findViewById(R.id.pickDate);
+        Calendar calendar=Calendar.getInstance();
+        pickDate.init(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH), new DatePicker.OnDateChangedListener() {
+            @Override
+            public void onDateChanged(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                birthDay.setText(dayOfMonth+"/"+monthOfYear+"/"+year);
+            }
+        });
+
+        mBuilder.setView(mView);
+        AlertDialog dialog=mBuilder.create();
+        dialog.show();
+    }
+
+
 }
 
